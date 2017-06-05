2b3f084334ec8e24621886e74e48f8ae6dd2f00f
Dallin Johnson <dallin.b.johnson@gmail.com>
2017-06-05 16:48:04 -0600

    Created seeds file for sample data

--- db/schema.rb
+++ db/schema.rb
@@ -22,13 +22,6 @@ ActiveRecord::Schema.define(version: 20170603165205) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "skills", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "percent_utilized"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
-  end
-
   create_table "portfolios", force: :cascade do |t|
     t.string   "title"
     t.string   "subtitle"
@@ -37,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170603165205) do
     t.text     "thumb_image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "percent_utilized"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
 end
