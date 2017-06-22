d4d7c08342776fd665e288d960f6da496d39e55c
Dallin Johnson <dallin.b.johnson@gmail.com>
2017-06-21 20:11:27 -0600

    Fixed About Us files to be About Me

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
 
-  get 'about-us', to: 'pages#about'
+  get 'about-me', to: 'pages#about'
   get 'contact', to: 'pages#contact'
 
   resources :blogs do
